c71a4970f24d086c23c4dce09402bb243b72b2a7
Rico Zhu <ricozhuthegreat@hotmail.com>
2021-09-08 21:36:55 -0400

    Created Model Framework

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 
+'''
+    Rico Zhu - September 8th, 2021 @ Duke University
+    
+    Pytorch implementation of a DCGAN and a StyleGAN
+
+'''
+
 import os
 import random
 
@@ -7,4 +14,14 @@ import matplotlib as plt
 import torch
 import torch.nn as nn
 import torch.optim as optim
+
+class Generator(nn.Module):
+
+    def __init__(self, *args):
+        super(Generator, self).__init__()
+
+class Discrimator(nn.Module):
+
+    def __init__(self, *args):
+        super(Discrimator, self).__init__()
 
