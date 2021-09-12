727c73c157c1b921c25e56fe3ec0410df26dd6fa
Rico Zhu <ricozhuthegreat@hotmail.com>
2021-09-12 16:32:44 -0400

    Added Weight Initialization

--- dcgan.py
+++ dcgan.py
@@ -15,6 +15,15 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
+# Function to initialize a given weight to a normal distribution
+def weights_init_normal(m):
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find("BatchNorm2d") != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0.0)
+
 class Generator(nn.Module):
 
     def __init__(self, img_dim, latent_dim, conv_dim, img_channels, rgb=True, **kwargs):
@@ -86,3 +95,18 @@ class Discrimator(nn.Module):
         validity = self.adv_layer(out)
 
         return validity
+
+def train(epochs, batch_size, b1, b2, sample_interval):
+
+    # Loss function
+    adversarial_loss = nn.BCELoss()
+
+    # Size of each image dimension, dimensionality of the latent space, size of convolution, number of image channels
+    generator = Generator(32, 100, 128, 1)
+    discriminator = Discrimator(32, 128, 1)
+
+    # Initialize weights
+    generator.apply(weights_init_normal)
+    discriminator.apply(weights_init_normal)
+
+
